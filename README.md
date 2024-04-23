e11f9b10e6b88b357bbe02596eb33a47dc607ffb
Nduke Sam <diamondsam33@gmail.com>
2024-04-23 07:50:27 +0100

    after a few setbacks, we forge ahead

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the repo detailing the follow-ups of my learning journey from 20.04.24
+
+Be in sync with your code
+
+Understand your code, let your code understand you 😌
--- index.js
+++ index.js
@@ -10,16 +10,16 @@
     // }
 
 // Factory function
-    function createCircle(radius) {
-        return {
-            radius,
-            draw: function() {
-                console.log('draw');
-            }
-        };
-    }
+    // function createCircle(radius) {
+    //     return {
+    //         radius,
+    //         draw: function() {
+    //             console.log('draw');
+    //         }
+    //     };
+    // }
 
-    let circle = createCircle(1);
+    // const circle = createCircle(1);
 
     //Constructor function 
     function Circle(radius) {
@@ -28,4 +28,44 @@
             console.log(draw);
         }
     }
-    const another = new Circle(1);
+ const another = new Circle(1);
+
+    //Add or remove properties
+     another.location = {x: 1};
+     another['finesse'] = 'fla';
+
+     delete another.finesse;
+
+     //Reiterate over or enumerate a property
+
+     for (const mkpo in another) {
+        console.log(mkpo, another[mkpo])
+    }
+
+    // const Circle1 = new Function ('radius', `
+    // this.radius = radius;
+    // this.draw = function () {
+    //     console.log(draw);
+    // }
+    // `);
+
+    // const secCircle = new Circle1(1);
+    
+   
+
+    //Primititves are copied by their value
+    //Objects or reference types are copied by their reference
+    // let x = { value: 10};
+    // let y = x;
+
+    // x.value = 40;
+
+    let obj = {value: 10};
+    let increase = function(obj) {
+        obj.value++
+    }
+
+    increase(obj);
+    console.log(obj);
+
+
